893a75f29c6d241cf715dedd3230678c80bfe961
AlexisRenchon <a.renchon@gmail.com>
2020-02-06 12:04:54 -0600

    Tweaks to Makie figure

--- Makie_DAMM.jl
+++ Makie_DAMM.jl
@@ -6,9 +6,16 @@ include("DAMM.jl")
 # Load parameters
 include("DAMM_param.jl") 
 
-Tsoil_range = 10.0:0.5:40.0
-Msoil_range = 0.05:0.01:0.35
+x = Tsoil_range = 10.0:0.5:35.0
+y = Msoil_range = 0.0:0.01:0.38
 
-Rsoil_DAMM = Float64[DAMM(Tsoil, Msoil) for Tsoil in Tsoil_range, Msoil in Msoil_range]
+z = Rsoil_DAMM = Float64[DAMM(Tsoil, Msoil) for Tsoil in Tsoil_range, Msoil in Msoil_range]
 
-scene = surface(Tsoil_range, Msoil_range, Rsoil_DAMM)
+scene = surface(x, y, z, colormap = :lighttest, transparency = true, alpha = 0.1, shading = false)
+wireframe!(scene, x, y, z, overdraw = true, transparency = true, color = (:black, 0.05))
+scale!(scene, 5, 200, 5) # Currently needed to rescale Msoil, because its range (00.5 - 0.35) is smaller than Tsoil and Rsoil range
+center!(scene)
+
+axis3D = scene[Axis]
+axis3D[:ticks][:textsize] = (2000.0,2000.0,2000.0)
+
