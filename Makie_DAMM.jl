504568738d41de4ccd664efe324c8c8b8539b6e8
AlexisRenchon <a.renchon@gmail.com>
2020-02-07 15:58:57 -0600

    update

--- Makie_DAMM.jl
+++ Makie_DAMM.jl
@@ -9,7 +9,7 @@ include("DAMM_param.jl")
 x = Tsoil_range = 10.0:0.5:35.0
 y = Msoil_range = 0.0:0.01:0.38
 
-scene, layout = layoutscene(20, 20, 30, resolution = (1000, 500))
+scene, layout = layoutscene(20, 20, 30, resolution = (700, 500))
 
 Text_date = layout[1,1:5] = LText(scene, text= "kMSx", textsize=20)
 sl_1 = layout[2, 1:5] = LSlider(scene, range= 1e-8:1e-9:1e-6)
@@ -18,7 +18,7 @@ sl_2 = layout[4, 1:5] = LSlider(scene, range= 5e7:1e6:5e8)
 Text_date = layout[5,1:5] = LText(scene, text= "kMO2", textsize=20)
 sl_3 = layout[6, 1:5] = LSlider(scene, range= 1e-4:1e-5:1e-2)
 
-ax3D = layout[1:20, 7:20] = LRect(scene, visible = false);
+ax3D = layout[1:20, 5:20] = LRect(scene, visible = false);
 scene3D = Scene(scene, lift(IRect2D, ax3D.layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
 surface!(scene3D, x, y, lift((kMSx, AlphaSx, kMO2)->Float64[DAMM(Tsoil, Msoil, kMSx, AlphaSx, kMO2) for Tsoil in Tsoil_range, Msoil in Msoil_range], sl_1.value, sl_2.value, sl_3.value), colormap = :lighttest, transparency = true, alpha = 0.1, shading = false, limits = Rect(10, 0, 0, 25, 0.4, 20))
 wireframe!(scene3D, x, y, lift((kMSx, AlphaSx, kMO2)->Float64[DAMM(Tsoil, Msoil, kMSx, AlphaSx, kMO2) for Tsoil in Tsoil_range, Msoil in Msoil_range], sl_1.value, sl_2.value, sl_3.value), overdraw = true, transparency = true, color = (:black, 0.05))
@@ -32,4 +32,11 @@ axis3D[:ticks][:textsize] = (2000.0,2000.0,2000.0)
 
 scene
 
+# to record some interaction
+# record(scene, "test.gif") do io
+#      for i = 1:200
+#          sleep(0.1)     
+#          recordframe!(io) # record a new frame
+#      end
+#  end
 
