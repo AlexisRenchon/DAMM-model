b01f140fc21aeb33b133f9f3e2190451ffb983d7
AlexisRenchon <a.renchon@gmail.com>
2020-02-11 16:12:51 -0600

    Added LsqFit of DAMM to data + v2 script for cleaner Makie

---
+++ DAMM_LsqFit.jl
@@ -0,0 +1,33 @@
+# Least square error fit of the DAMM model to data
+using LsqFit, CSV
+
+# Load fixed param
+R = 8.314472*10^-3 # Universal gas constant
+Dl = 3.17 # Diffusion coeff of substrate in liquid phase
+Sxt = 0.0125 # Soil C content
+p = 2.4*10^-2 # Fraction of soil C that is considered soluble
+Dgas = 1.67 # Diffusion coefficient of oxygen in air
+Db = 1.53 # Soil bulk density !! DAMM complex if SWC > 0.365. old 1.5396
+Dp = 2.52 # Soil particle density
+
+# DAMM model, !for the algorithm to work, Param are scaled to be of similar magnitude
+@. multimodel(Ind_var, Param) = (1e6*Param[1]*exp(-1e1*Param[2]/(R*(273.15+Ind_var[:, 1]))))* 
+(((p*Sxt)*Dl*Ind_var[:, 2]^3)/(1e-8*Param[3]+((p*Sxt)*Dl*Ind_var[:, 2]^3)))* 
+((Dgas*0.209*(1-Db/Dp-Ind_var[:, 2])^[4/3])/ 
+(1e-3*Param[4]+(Dgas*0.209*(1-Db/Dp-Ind_var[:, 2])^[4/3])))* 
+10000*10/1000/12*1e6/60/60 
+
+# Example
+Param = Param_ini = [2.0,5.0,3.46,2.0] # Scaled Param, as explained above
+Ind_var = [25.0 0.3; 27 0.25]
+multimodel(Ind_var, Param)
+
+# Load data
+df = CSV.read("Input\\AUCum_Reco_Rsoil_soilvars_2014_2017.csv",dateformat="mm/dd/yyyy HH:MM")
+SWC_3 = df.SWC_R3; Tsoil_3 = df.Ts_R3; Rsoil_3 = Dep_var = df.Rsoil_R3
+Ind_var = hcat(Tsoil_3, SWC_3)
+
+# Fit DAMM to data
+# Need to load data first... TO DO
+fit = curve_fit(multimodel, Ind_var, Dep_var, Param_ini) # For DAMM, Ind_var is Tsoil and SWC, Dep_var is Rsoil
+
---
+++ Makie_DAMM_v2.jl
@@ -0,0 +1,40 @@
+using Makie, MakieLayout
+
+# Load DAMM function
+include("DAMM.jl")
+
+# Load parameters
+include("DAMM_param.jl") 
+
+x = Tsoil_range = 10.0:0.5:35.0
+y = Msoil_range = 0.0:0.01:0.38
+
+scene, layout = layoutscene()
+texts = Array{LText}(undef,5)
+sliders = Array{LSlider}(undef,5)
+sliders[1] = LSlider(scene, range= 1e-8:1e-8:1e-6)
+sliders[2] = LSlider(scene, range= 5e7:1e6:5e8)
+sliders[3] = LSlider(scene, range= 1e-4:1e-5:1e-2)
+sliders[4] = LSlider(scene, range= 58.0:0.2:70.0)
+sliders[5] = LSlider(scene, range= 0.005:0.001:0.02)
+texts[1] = LText(scene, text= lift(X->string("kMSx", " = ", round(X, sigdigits = 2)), sliders[1].value), textsize=15, width = Auto(false))
+texts[2] = LText(scene, text= lift(X->string("AlphaSx", " = ", X), sliders[2].value), textsize=15, width = Auto(false))
+texts[3] = LText(scene, text= lift(X->string("kMO2", " = ", X), sliders[3].value), textsize=15, width = Auto(false))
+texts[4] = LText(scene, text= lift(X->string("EaSx", " = ", X), sliders[4].value), textsize=15, width = Auto(false))
+texts[5] = LText(scene, text= lift(X->string("Sxtot", " = ", X, " g C cm-3"), sliders[5].value), textsize=15, width = Auto(false))
+vertical_sublayout = layout[1, 1] = vbox!(
+    Iterators.flatten(zip(texts, sliders))...;
+    width = 400, height = Auto(false))
+
+ax3D = layout[1, 2] = LRect(scene, visible = false);
+scene3D = Scene(scene, lift(IRect2D, ax3D.layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
+surface!(scene3D, x, y, lift((kMSx_v, AlphaSx_v, kMO2_v, EaSx_v, Sxtot_v)->Float64[DAMM(Tsoil, Msoil; kMSx = kMSx_v, AlphaSx = AlphaSx_v, kMO2 = kMO2_v, EaSx = EaSx_v, Sxtot = Sxtot_v) for Tsoil in Tsoil_range, Msoil in Msoil_range], sliders[1].value, sliders[2].value, sliders[3].value, sliders[4].value, sliders[5].value), colormap = :lighttest, transparency = true, alpha = 0.1, shading = false, limits = Rect(10, 0, 0, 25, 0.4, 20))
+wireframe!(scene3D, x, y, lift((kMSx_v, AlphaSx_v, kMO2_v, EaSx_v, Sxtot_v)->Float64[DAMM(Tsoil, Msoil; kMSx = kMSx_v, AlphaSx = AlphaSx_v, kMO2 = kMO2_v, EaSx = EaSx_v, Sxtot = Sxtot_v) for Tsoil in Tsoil_range, Msoil in Msoil_range], sliders[1].value, sliders[2].value, sliders[3].value, sliders[4].value, sliders[5].value), overdraw = true, transparency = true, color = (:black, 0.05))
+scale!(scene3D, 1, 40, 1) 
+center!(scene3D)
+axis3D = scene3D[Axis]
+axis3D[:ticks][:textsize] = (400.0,400.0,400.0)
+axis3D.names.axisnames = ("Tsoil", "SWC", "Rsoil")
+axis3D[:names][:textsize] = (400.0,400.0,400.0) # same as axis.names.textsize
+
+scene
